982a82625185c212d4eff2f4b9b484c2ede28574
s3rxio <cergotabagua222@gmail.com>
2024-03-04 16:52:25 +0300

    feat: add redux

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 const App: React.FC = () => {
-  return <>Hello World</>;
+  return <Provider store={store}>Hello World</Provider>;
 };
 
 export default App;
