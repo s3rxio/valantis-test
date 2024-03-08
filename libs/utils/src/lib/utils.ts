aae901c1e82be0b157df2167fd04b4fb0f6a29e7
s3rxio <cergotabagua222@gmail.com>
2024-03-08 16:33:36 +0300

    feat(utils): create lib

---
+++ libs/utils/src/lib/utils.ts
@@ -0,0 +1,8 @@
+export const generateUTCTimestamp = (date: Date) => {
+  const year = date.getUTCFullYear();
+  const month = addZero(date.getUTCMonth());
+  const day = addZero(date.getUTCDate());
+  return `${year}${month}${day}`;
+};
+
+export const addZero = (num: number) => (num < 10 ? `0${num}` : num);
